f022856c5df577a7a5a7b31a887fe2fb2c48a5cd
Eranga <erangakavishanka@icloud.com>
2026-01-14 19:33:09 +0530

    developed it services page

--- src/pages/it_services_page.jsx
+++ src/pages/it_services_page.jsx
@@ -1,13 +1,13 @@
-import GetInTouch from "./sections2/get-in-touch";
-import OurTestimonials from "./sections2/our-testimonials";
-import SubscribeNewsletter from "./sections2/subscribe-newsletter";
-import TrustedCompanies from "./sections2/trusted-companies";
-import Footer from "./components2/footer";
-import LenisScroll from "./components2/lenis-scroll";
-import Navbar from "./components2/navbar";
-import AboutOurApps from "./sections2/about-our-apps";
-import HeroSection from "./sections/hero-section";
-import OurLatestCreation from "./sections2/our-latest-creation";
+// import GetInTouch from "../sections2/get-in-touch";
+import OurTestimonials from "../sections2/our-testimonials";
+import SubscribeNewsletter from "../sections2/subscribe-newsletter";
+import TrustedCompanies from "../sections2/trusted-companies";
+import Footer from "../components2/footer";
+import LenisScroll from "../components2/lenis-scroll";
+import AboutOurApps from "../sections2/about-our-apps";
+import HeroSection from "../sections2/hero-section";
+import OurLatestCreation from "../sections2/our-latest-creation";
+
 
 
 export default function ITServicesPage(){
@@ -15,7 +15,7 @@ export default function ITServicesPage(){
    return (
           <>
               <LenisScroll />
-              <Navbar />
+             
               <main className="px-6 md:px-16 lg:px-24 xl:px-32">
                   <HeroSection />
                   <OurLatestCreation />
